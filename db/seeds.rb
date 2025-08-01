5c7fc6aa9c3ee183c6595a572f15dc733b1e5f72
Nico Lauria <nicolauria1988@gmail.com>
2025-08-01 11:45:47 -0700

    add factory bot to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,32 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'factory_bot_rails'
+require 'faker'
+
+FactoryBot.define do
+  factory :user, class: User do
+    name { Faker::Name.name }
+    email { Faker::Internet.safe_email }
+    phone { Faker::PhoneNumber.phone_number }
+    password_digest { "password100" }
+    street { Faker::Address.street_address }
+    city { Faker::Address.city }
+    state { Faker::Address.state }
+    country { Faker::Address.country }
+    zip_code { Faker::Address.zip_code }
+  end
+
+  factory :location do
+    association :owner_id, factory: :user
+    street { Faker::Address.street_address }
+    city { Faker::Address.city }
+    state { Faker::Address.state }
+    country { Faker::Address.country }
+    zip_code { Faker::Address.zip_code }
+    category { ["Home", "Apartment", "Guest House", "Room"].sample }
+    price { Faker::Number.between(from: 100, to: 1000) }
+    notes { Faker::HTML.paragraph(sentence_count: 10) }
+  end
+end
